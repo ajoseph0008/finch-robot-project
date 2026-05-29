67e8ca6680f1902c8c70658659beb4c650593906
ajoseph0008 <ajoseph0008@mymail.lausd.net>
2026-05-29 11:53:28 -0700

    Check point 1

--- src/main/java/com/hello/FinalFinch.java
+++ src/main/java/com/hello/FinalFinch.java
@@ -6,4 +6,25 @@ public class FinalFinch extends Finch{
     public FinalFinch(String device){
         super(device);
     }
+    public void cP1(){
+        for(int a = 0; a<8; a++){
+        while(getDistance()> 10){
+        setMove("F",5,99);
+        }
+        if(a == 0|| a==1||a==4||a==5){
+            setTurn("L", 90, 99);
+        } else {
+            setTurn("R", 90, 99);
+        }
+
+       // if(b == 0)
+      //  setTurn("L", 90, )
+       // }
+
+
+        
+        
+        
+    }
+}
 }
--- src/main/java/com/hello/TesterFinch.java
+++ src/main/java/com/hello/TesterFinch.java
@@ -8,7 +8,15 @@ public class TesterFinch{
         bird.setTail(3,0,0,100);
         bird.setTail(2,0,0,100);
         bird.setTail(4,0,100,100);
-        bird.playNote(60,16);
-        bird.setMove("F",100,99);
+        
+       // while(bird.getDistance()>10){
+       // bird.setMove("F",5,50);
+      //  }
+
+        //bird.playNote(60,16);
+        //bird.setMove("F",10,99);
+        bird.cP1();
+
+        // bird.stopAll();
     }
 }
